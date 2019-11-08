8c27468b0b7e1f4be955f1123a497c158738b265
Maggie Johnson <mmjohn09@icloud.com>
2019-11-08 14:23:37 -0600

    Finished 1-4

--- BangazonSQLExercise.sql
+++ BangazonSQLExercise.sql
@@ -1,3 +1,9 @@
-﻿SELECT e.FirstName, e.LastName, e.IsSupervisor, d.Name AS DeptName FROM Employee e
-LEFT JOIN Department d ON d.Id = e.DepartmentId
-ORDER BY d.Name, e.LastName, e.FirstName;
+﻿--SELECT e.FirstName, e.LastName, e.IsSupervisor, d.Name AS DeptName FROM Employee e
+--LEFT JOIN Department d ON d.Id = e.DepartmentId
+--ORDER BY d.Name, e.LastName, e.FirstName;
+
+--SELECT * FROM Department ORDER BY Budget DESC;
+
+--SELECT e.FirstName, e.LastName, e.isSupervisor, d.Name AS DeptName FROM Employee e
+--INNER JOIN Department d ON d.Id = e.DepartmentId
+--WHERE e.IsSupervisor = 1;
