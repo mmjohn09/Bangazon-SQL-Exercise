67ad8965a7782112a5dae73e4de0c667fdd932c0
Maggie Johnson <mmjohn09@icloud.com>
2019-11-08 15:13:14 -0600

    completed 5-6

--- BangazonSQLExercise.sql
+++ BangazonSQLExercise.sql
@@ -7,3 +7,12 @@
 --SELECT e.FirstName, e.LastName, e.isSupervisor, d.Name AS DeptName FROM Employee e
 --INNER JOIN Department d ON d.Id = e.DepartmentId
 --WHERE e.IsSupervisor = 1;
+
+--SELECT e.DepartmentId, d.Name AS DepartmentName, COUNT(e.DepartmentId) AS NumberOfEmployees FROM Employee e
+--JOIN Department d ON e.DepartmentId = d.Id
+--GROUP BY d.Name, e.DepartmentId;
+
+--UPDATE Department SET Budget = (Budget * 1.20);
+
+--List the employee full names for employees who are not signed up for any training programs.
+SELECT * FROM TrainingProgram;
